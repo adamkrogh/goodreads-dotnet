8fc43dad38c7a366dcbf00720b5f5ab3b323ced5
Adam Krogh <adamkrogh@gmail.com>
2016-07-11 20:27:55 -0600

    Wrote some more tests for reviews.

--- Goodreads.Tests/Clients/ReviewsClientTests.cs
+++ Goodreads.Tests/Clients/ReviewsClientTests.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Goodreads.Clients;
+using Goodreads.Models.Request;
+using Goodreads.Models.Response;
 using Xunit;
 
 namespace Goodreads.Tests.Clients
@@ -12,11 +10,13 @@ namespace Goodreads.Tests.Clients
     {
         private readonly IReviewsClient ReviewsClient;
         private readonly int UserId;
+        private readonly int ReviewsUserId;
 
         public ReviewsClientTests()
         {
             ReviewsClient = Helper.GetAuthClient().Reviews;
             UserId = Helper.GetUserId();
+            ReviewsUserId = 7284465;
         }
 
         public class TheGetListByUserMethod : ReviewsClientTests
@@ -24,9 +24,43 @@ namespace Goodreads.Tests.Clients
             [Fact]
             public async Task ReturnsBooks()
             {
-                var books = await ReviewsClient.GetListByUser(UserId);
+                var books = await ReviewsClient.GetListByUser(ReviewsUserId);
 
                 Assert.NotNull(books);
+            }
+
+            [Fact]
+            public async Task ReturnsCorrectPageSize()
+            {
+                var expectedPageSize = 42;
+                var books = await ReviewsClient.GetListByUser(ReviewsUserId, pageSize: expectedPageSize);
+
+                Assert.NotNull(books);
+                Assert.Equal(books.Pagination.End, expectedPageSize);
+                Assert.Equal(books.List.Count, expectedPageSize);
+            }
+
+            [Fact]
+            public async Task ReturnsSortedList()
+            {
+                var reviews = await ReviewsClient.GetListByUser(ReviewsUserId, sort: SortReviewsList.AverageRating, order: Order.Descending);
+
+                Assert.NotNull(reviews);
+
+                Review previousReview = null;
+                foreach (var currentReview in reviews.List)
+                {
+                    Assert.NotNull(currentReview);
+                    Assert.NotNull(currentReview.Book);
+                    Assert.NotNull(currentReview.Book.AverageRating);
+
+                    if (previousReview != null)
+                    {
+                        Assert.True(previousReview.Book.AverageRating >= currentReview.Book.AverageRating);
+                    }
+
+                    previousReview = currentReview;
+                }
             }
         }
     }

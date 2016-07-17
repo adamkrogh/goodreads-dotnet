8a8c75a82efad92c5b2512ddcb3fc5542d860029
Adam Krogh <adamkrogh@gmail.com>
2016-07-16 19:01:12 -0600

    Fixed up the edit date read test.

--- Goodreads.Tests/Clients/ReviewsClientTests.cs
+++ Goodreads.Tests/Clients/ReviewsClientTests.cs
@@ -192,7 +192,7 @@ namespace Goodreads.Tests.Clients
             {
                 var reviewBeforeEdit = await ReviewsClient.GetById(EditReviewId);
                 var dateBeforeEdit = reviewBeforeEdit.DateRead;
-                var expectedNewDate = dateBeforeEdit.Value.Date == DateTime.Today ? DateTime.Today.AddDays(-1) : DateTime.Today;
+                var expectedNewDate = dateBeforeEdit.Value.Date == DateTime.UtcNow.Date ? DateTime.UtcNow.Date.AddDays(-1) : DateTime.UtcNow.Date;
 
                 var result = await ReviewsClient.Edit(EditReviewId, dateRead: expectedNewDate);
 

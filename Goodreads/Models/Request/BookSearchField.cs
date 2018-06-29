bde229373bf4e7bf21dc1a000d455ebf845391b4
iamyoshita <35068136+iamyoshita@users.noreply.github.com>
2018-06-29 18:40:27 +0530

    Update BookSearchField.cs

--- Goodreads/Models/Request/BookSearchField.cs
+++ Goodreads/Models/Request/BookSearchField.cs
@@ -24,7 +24,7 @@ namespace Goodreads.Models.Request
         /// Search by the author of the book.
         /// </summary>
         [QueryParameterValue("author")]
-        Author
+        Author,
         /// <summary>
         /// Search by the genre of the book.
         /// </summary>
